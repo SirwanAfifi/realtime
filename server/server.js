dea531dedae88538e0a2fc737e8720c0f93fd0df
Sirwan Afifi <sir1afifi@gmail.com>
2019-12-06 23:29:35 +0330

    Add basic setup

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/stream", (req, res, next) => {
+  const headers = {
+    "Content-Type": "text/event-stream",
+    Connection: "keep-alive",
+    "Cache-Control": "no-cache"
+  };
+  res.writeHead(200, headers);
+
+  // Send data
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`The server is listening on port ${PORT}`));
