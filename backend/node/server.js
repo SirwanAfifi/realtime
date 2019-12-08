f05c4c4494df1d515d9562fe552ebc6fd29f743a
Sirwan Afifi <sir1afifi@gmail.com>
2019-12-08 11:11:34 +0330

    Add mongoose connection

--- backend/node/server.js
+++ backend/node/server.js
@@ -1,5 +1,17 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE;
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(con => console.log("DB connection successful!"));
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
