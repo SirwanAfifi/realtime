0ce5216f5e6d66d1a907bd725ebfb54cd0f39c6e
Sirwan Afifi <sir1afifi@gmail.com>
2019-12-08 11:02:40 +0330

    Add dotenv

--- backend/node/server.js
+++ backend/node/server.js
@@ -1,3 +1,5 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -12,6 +14,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`The server is listening on port ${PORT}`));
